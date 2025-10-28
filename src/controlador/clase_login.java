afe8bd16b97a7c2f0d9a94f3103bc852a9fffc3a
Asus <Asus@DESKTOP-2MH6881>
2025-10-28 15:54:30 -0600

    creacion de la clase_login

---
+++ src/controlador/clase_login.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author Asus
+ */
+public class clase_login {
+    //usuario y contraseña harcodeados para que el boton de entrar de la ventana inicio_admin haga las validaciones
+    String user_admin="admin";
+    String password_admin="123";
+    public String nombre_admin(){
+    return user_admin;
+    }
+    public String contra_admin(){
+    return password_admin;
+    }
+}
